e42b2a5c4b2d7ad5cef5557395f70cf856603b95
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2020-11-13 17:42:44 -0800

    basic android build running

---
+++ Assets/ARTapToPlaceObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    private ARRaycastManager aRRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        // find center of screen
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f,0.5f));
+        // create list of raycast hits
+        var hits = new List<ARRaycastHit>();
+
+        // send a ray from center of screen forward to find any planes
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        // placement pose is valid if a plane is hit
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            // set placement pose to the first plane in list
+            placementPose = hits[0].pose;
+        }
+    }
+}
