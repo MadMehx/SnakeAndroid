7c5245cd3fef945806f6f08ce0525a3c5a47b9e7
Nephrenia <vincentbui67@gmail.com>
2020-12-09 15:39:43 -0800

    Pause Menu
    Button on top left but hard to press so maybe making the button bigger will be beneficial. When you tap on the pause button, it will bring you to a new scene where the user can resume the game or quit the game.

---
+++ Assets/pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour
+{
+    //-----------------------------------------------------------------------------
+    // PlayGame
+    // public void function. This function allows the user to press on the start
+    // button for the application and it will load the game. The function 
+    // uses the scenemanager in Unity to change between different scenes within
+    // the application.
+    public void PauseGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //-----------------------------------------------------------------------------
+    // ResumeGame
+    // public void function. This function allows the user to press on the start
+    // button for the application and it will load the game. The function 
+    // uses the scenemanager in Unity to change between different scenes within
+    // the application.
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    //-----------------------------------------------------------------------------
+    // QuitGame
+    // public void function. This function allows the user to press on the quit
+    // button for the application and it will quit the game. After the user quits
+    // the application, the application should close and return the user back to
+    // their home screen or their list of applications.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
