b8d4ae236d89ff0f25c33a04bacf2b9d73872438
MadMehx <65829543+MadMehx@users.noreply.github.com>
2020-12-09 17:13:17 -0800

    hit boss

--- Assets/ClickAnim.cs
+++ Assets/ClickAnim.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//[RequiredComponent(typeof(Rigidbody))]
+
+
 public class ClickAnim : MonoBehaviour
 {
 
     private Animator animator;
     private Animation animation;
     int tap = 0;
+    protected Rigidbody r;
+    public float Speed;
+    public float AngularSpeed;
+
+
     void Start()
     {
-        Physics.gravity = new Vector3(0, -10.0F, 0);
+        Physics.gravity = new Vector3(0, -100.0F, 0);
+        //r = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -35,7 +45,12 @@ public class ClickAnim : MonoBehaviour
                 tap++;
                 Debug.Log("tap++");
                 animator.enabled = false;
-                Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+                //Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+                //Speed = r.velocity.magnitude;
+                //AngularSpeed = r.angularVelocity.magnitude;
+
+                //r.Addforce(Vector3.forward);
+                Physics.gravity = new Vector3(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
             }
             else
             {
